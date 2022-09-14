32b6a4d6198e3490621ad978159aaf3f41a45225
Jabee <imamichi0419@gmail.com>
2022-09-15 01:03:45 +0900

    last

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <button class="reset" type="button" v-on:click="reset">reset all</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import firebaseApp from "../firebase.js"
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  setDoc,
+  doc
+} from "firebase/firestore";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+
+    methods: {
+      reset: function() {
+        const db = getFirestore(firebaseApp);
+      
+        // for(var i = 0; i < 5; i++) {
+        //   const ref = doc(db, "stage1", (i + 1))
+        //   const data = {answer: "◯"}
+        //   setDoc(ref, data)
+        // }
+
+        for(var j = 0; j < 5; j++) {
+          const ref = doc(db, "rate", "team" + (j + 1))
+          const data = {rate: 100}
+
+          setDoc(ref, data)
+        }
+
+        for(var k = 0; k < 5; k++) {
+          const ref = doc(db, "team", "team" + (k + 1))
+          const data = {score: 0}
+
+          setDoc(ref, data)
+        }
+
+      }
+    }
   }
-}
 </script>
